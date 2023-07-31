1b14656b090988018a5b3f801b95a58eb76cfca7
Leckller <7leckler@gmail.com>
2023-07-31 09:55:08 -0300

    helding

--- script.js
+++ script.js
@@ -15,57 +15,81 @@ function geradorDeBola(x, y, color, raio, sentido = true) {
 
 const bolasCriadas = []
 
-cnv.addEventListener('mousedown', () => {
-    
+let selected
+
+cnv.addEventListener('mousedown', ({ offsetX, offsetY }) => {
+    bolasCriadas.find(({ x, y, raio, helding, cor }) => {
+        const cat1 = x - offsetX
+        const cat2 = y - offsetY
+        const hyp = Math.sqrt((cat1 ** 2) + (cat2 ** 2))
+        if (hyp < raio) selected = cor, bolasCriadas.find((e) => {
+            if (e.cor === selected) e.helding = true
+        })
+    })
+
 })
 
-cnv.addEventListener('mousedown', () => {})
+cnv.addEventListener('mouseup', () => {
+    for (let i = 0; i < bolasCriadas.length; i += 1) {
+        bolasCriadas[i].helding = false
+    }
+})
 
-cnv.addEventListener('mousedown', () => {})
+cnv.addEventListener('mousemove', ({ offsetX, offsetY }) => {
+    for (let i = 0; i < bolasCriadas.length; i += 1) {
+        const e = bolasCriadas[i]
+        if (e.helding) {
+            e.x = offsetX
+            e.y = offsetY
+        }
+    }
+})
 
 function geradorAutomatico(quantidade, sentido = true) {
     for (let i = 0; i < quantidade; i += 1) {
         const raio = Math.round(Math.random() * 90 + 10)
         let x = Math.floor(Math.random() * cnv.width)
         let y = Math.floor(Math.random() * cnv.height)
-        const vtX = Math.floor(Math.random() * 10+ 1)
-        x = x - raio < 0 ? x =+ raio : x
-        y = y - raio < 0 ? y =+ raio : y
+        const vtX = Math.floor(Math.random() * 10 + 1)
+        x = x - raio < 0 ? x = + raio : x
+        y = y - raio < 0 ? y = + raio : y
         x = x + raio > cnv.width ? x -= raio : x
         y = y + raio > cnv.width ? y -= raio : y
         const cor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`
-        bolasCriadas.push({x, y, raio, cor, vtX, vtY: 0})
+        bolasCriadas.push({ x, y, raio, cor, vtX, vtY: 0, helding: false })
         geradorDeBola(x, y, cor, raio, sentido)
     }
 }
 
 console.log(bolasCriadas)
 
-function recreate () {
-    ctx.clearRect(0,0,cnv.width,cnv.height)
+function recreate() {
+    ctx.clearRect(0, 0, cnv.width, cnv.height)
     bolasCriadas.forEach(({ x, y, raio, cor }) => {
         geradorDeBola(x, y, cor, raio, true)
-        
+
     })
 }
 
-function move () {
+function move() {
     for (let i = 0; i < bolasCriadas.length; i += 1) {
         const ind = bolasCriadas[i]
-        ind.vtY += gravidade
-        ind.y += ind.vtY
-        ind.x += ind.vtX
-        if ((ind.y + ind.raio) > cnv.height) ind.y = cnv.height - ind.raio, ind.vtY *= -1, ind.vtY += ind.y / 1000
-        if ((ind.x + ind.raio) > cnv.width || (ind.x - ind.raio) < 0) {
-            if ((ind.x - ind.raio) < 0) ind.x = ind.raio, ind.x += ind.vtX *= -1
-            if ((ind.x + ind.raio) > cnv.width) ind.x = cnv.width - ind.raio, ind.x += ind.vtX *= -1
+        if (!ind.helding) {
+            ind.vtY += gravidade
+            ind.y += ind.vtY
+            ind.x += ind.vtX
+            if ((ind.y + ind.raio) > cnv.height) ind.y = cnv.height - ind.raio, ind.vtY *= -1, ind.vtY += ind.y / 1000
+            if ((ind.x + ind.raio) > cnv.width || (ind.x - ind.raio) < 0) {
+                if ((ind.x - ind.raio) < 0) ind.x = ind.raio, ind.x += ind.vtX *= -1
+                if ((ind.x + ind.raio) > cnv.width) ind.x = cnv.width - ind.raio, ind.x += ind.vtX *= -1
+            }
         }
     }
 }
 
-geradorAutomatico(100, true)
+geradorAutomatico(200, true)
 
-function loop () {
+function loop() {
     window.requestAnimationFrame(loop)
     recreate()
     move()
